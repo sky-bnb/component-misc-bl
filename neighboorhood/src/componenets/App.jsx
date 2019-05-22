a1a25b92ff4da4f5345614321f94d60677183718
bjlagman12 <bjlagman@gmail.com>
2019-05-22 15:34:21 -0700

    Implement basic skeleton

--- neighboorhood/src/componenets/App.jsx
+++ neighboorhood/src/componenets/App.jsx
@@ -12,9 +12,49 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="container">
+      <section className="hostProfile">
+        <div className="host">
+          <div className="left">
+            <div className="top">
+            <h2>Hosted by Living The Dream</h2>
+            </div>
+            <div className="bottom">
+              <div class="_czm8crp">Joined in February 2017</div>
+            </div>
+          </div>
+          <div className="right">
+            <img src="https://cdn.acidcow.com/pics/20170117/pretty_girls_02.jpg" />
+          </div>
+        </div>
+        <div>
+       <div>
+           reviews
+          </div>
+          <div>
+            verified
+          </div>
+
+        </div>
+        <div>
+
+        </div>
+        <div>
+
+        </div>
+        <div>
+          button
+        </div>
+        <div>
+          border
+        </div>
+        <div>
+          bottom
+        </div>
+        <div>
+          border fotter
+        </div>
         testing
-      </div>
+      </section>
     );
   }
 }
